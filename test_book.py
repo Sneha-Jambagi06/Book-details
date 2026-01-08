abed0c38c6aee6f45fba1be824a7e5d250a037b9
Sneha-Jambagi06 <snehajambagi2020@gmail.com>
2026-01-08 17:06:05 +0530

    Update test_book.py

--- test_book.py
+++ test_book.py
@@ -1,9 +1,10 @@
 from book import book_info
 def test_book_info(bookID,bookTitle,authorName,yearOfPublication):
     expected_output = (
-        "Book ID=101\n"
-        "Book Title=Geetaanjali\n"
-        "Author Name=Rabindranath Tagore\n"
-        "Year of Publication=1986"
+        "bookID=101\n"
+        "bookTitle=Geetaanjali\n"
+        "authorName=Rabindranath Tagore\n"
+        "yearofPublication=1986"
     )
     assert (101,"Geetaanajali","Rabindranath Tagore",1986) == expected_output
+     

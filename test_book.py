9ae10c072398001a4e80f3d369da77ea5f4c42a4
Sneha-Jambagi06 <snehajambagi2020@gmail.com>
2026-01-08 16:38:40 +0530

    Update test_book.py

--- test_book.py
+++ test_book.py
@@ -1,31 +1,9 @@
-def test_book_case1():
-    result = book_info(101, "Geetanjali", "Rabindranath Tagore", 1986)
-    expected = (
+from book import book_info
+def test_book_info(bookID,bookTitle,authorName,yearOfPublication):
+    expected_output = (
         "Book ID=101\n"
-        "Book Title=Geetanjali\n"
+        "Book Title=Geetaanjali\n"
         "Author Name=Rabindranath Tagore\n"
         "Year of Publication=1986"
     )
-    assert result == expected
-
-
-def test_book_case2():
-    result = book_info(102, "Wings of Fire", "A. P. J. Abdul Kalam", 1999)
-    expected = (
-        "Book ID=102\n"
-        "Book Title=Wings of Fire\n"
-        "Author Name=A. P. J. Abdul Kalam\n"
-        "Year of Publication=1999"
-    )
-    assert result == expected
-
-
-def test_book_case3():
-    result = book_info(103, "The Alchemist", "Paulo Coelho", 1988)
-    expected = (
-        "Book ID=103\n"
-        "Book Title=The Alchemist\n"
-        "Author Name=Paulo Coelho\n"
-        "Year of Publication=1988"
-    )
-    assert result == expected
+    assert (101,"Geetaanajali","Rabindranath Tagore",1986) == expected_output

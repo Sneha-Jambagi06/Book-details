e991a5f0f172930df5c10e6e7bf5220ba27b9fd6
Sneha-Jambagi06 <snehajambagi2020@gmail.com>
2026-01-08 16:34:49 +0530

    Update book.py

--- book.py
+++ book.py
@@ -1,15 +1,33 @@
-def book_info(bookID,bookTitle,authorName,yearOfPublication):
-  return(f"Book Id:{bookID}\n"
-         f"Book Title:{bookTitle}\n"
-         f"Author Name:{authorName}\n"
-         f"Year of Publication:{yearOfPublication}\n"
-)
-  
-if __name__=="__main__":
-      bookID=101
-      bookTitle="Geetaanjali"
-      authorName="Rabindranath Tagore"
-      yearOfPublication=1986
-      print("Book Details:")
-      print(book_info(bookID,bookTitle,authorName,yearOfPublication))
-      
+from book import book_info
+
+def test_book_case1():
+    result = book_info(101, "Geetanjali", "Rabindranath Tagore", 1986)
+    expected = (
+        "Book ID=101\n"
+        "Book Title=Geetanjali\n"
+        "Author Name=Rabindranath Tagore\n"
+        "Year of Publication=1986"
+    )
+    assert result == expected
+
+
+def test_book_case2():
+    result = book_info(102, "Wings of Fire", "A. P. J. Abdul Kalam", 1999)
+    expected = (
+        "Book ID=102\n"
+        "Book Title=Wings of Fire\n"
+        "Author Name=A. P. J. Abdul Kalam\n"
+        "Year of Publication=1999"
+    )
+    assert result == expected
+
+
+def test_book_case3():
+    result = book_info(103, "The Alchemist", "Paulo Coelho", 1988)
+    expected = (
+        "Book ID=103\n"
+        "Book Title=The Alchemist\n"
+        "Author Name=Paulo Coelho\n"
+        "Year of Publication=1988"
+    )
+    assert result == expected
